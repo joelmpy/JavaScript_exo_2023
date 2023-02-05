4a38e916edeabaa06cf76c95c3a9ad8b28baf26e
joel <joel_mpy@hotmail.com>
2023-02-05 16:47:06 +0100

    first commit

---
+++ 01_exercice_2023/main.js
@@ -0,0 +1,15 @@
+const person = {
+firstname: "John",
+  lastName: "Doe",
+  age: 24,
+  note: [12, 15, 21],
+  job: { name: "Cuisinier", hours: 35 },
+};
+
+
+const age = 20
+if (age === 18) {
+    console.log('Bonjour Vous etes majeur');
+} else {
+    console.log('Bonjour vous etes pas majeur');
+}
