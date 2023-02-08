4c67d08a01a202b208b3bd0e97ef8cfb251ee62b
joel <joel_mpy@hotmail.com>
2023-02-08 10:35:57 +0100

    exo-js

--- 01_exercice_2023/js/exo-2.js
+++ 01_exercice_2023/js/exo-2.js
@@ -15,10 +15,41 @@ function slideShow() {
 
 window.onload = slideShow;
 
-let episodeTitle = "Breaking Bad";
-let episodeDuration = 50;
-let hasBeenWatched = false;
 
-document.querySelector("#episode-info").innerText = `Episode: ${episodeTitle}
-Duration: ${episodeDuration} min
-${hasBeenWatched ? "Already watched" : "Not yet watched"}`;
+
+class Episode {
+  constructor(title, duration, hasBeenWatched) {
+    this.title = title;
+    this.duration = duration;
+    this.hasBeenWatched = hasBeenWatched;
+  }
+}
+
+let firstEpisode = new Episode("Dark Beginnings", 45, true);
+let secondEpisode = new Episode("The Mystery Continues", 45, false);
+let thirdEpisode = new Episode("The Unexpected Climax", 60, false);
+
+document.querySelector("#first-episode-info").innerText = `Episode: ${
+  firstEpisode.title
+}
+  Duration: ${firstEpisode.duration} min
+  ${firstEpisode.hasBeenWatched ? "Already watched" : "Not yet watched"}`;
+
+document.querySelector("#second-episode-info").innerText = `Episode: ${
+  secondEpisode.title
+}
+  Duration: ${secondEpisode.duration} min
+  ${secondEpisode.hasBeenWatched ? "Already watched" : "Not yet watched"}`;
+
+document.querySelector("#third-episode-info").innerText = `Episode: ${
+  thirdEpisode.title
+}
+  Duration: ${thirdEpisode.duration} min
+  ${thirdEpisode.hasBeenWatched ? "Already watched" : "Not yet watched"}`;
+
+
+let div = document.createElement("div");
+let p = document.createElement("p");
+div.append("Du texte", p);
+
+console.log(div)
